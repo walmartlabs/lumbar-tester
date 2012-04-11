3425c5891441c1e6a0bbe427e5dce50976bf0b3d
kpdecker <kpdecker@gmail.com>
2012-04-11 01:08:19 -0500

    Basic module inlining test

---
+++ test/expected/test-modules/base.js
@@ -0,0 +1,12 @@
+var Lumbar;
+Lumbar = (function() {
+var module = {exports: {}};
+var exports = module.exports;
+var bar;
+;;
+(function() {
+test();
+;;
+})();
+return module.exports;
+}).call(this);
---
+++ test/expected/test-modules/home.js
@@ -0,0 +1,11 @@
+Lumbar['home'] = (function() {
+var module = {exports: {}};
+var exports = module.exports;
+var home = 1;
+;;
+(function() {
+test();
+;;
+})();
+return module.exports;
+}).call(this);
