79cde1b1de5a6e0bca48739725f96034bcde7d46
kpdecker <kpdecker@gmail.com>
2012-10-15 21:20:54 -0500

    Convert tests to mocha

--- test/lumbar-tester.js
+++ test/lumbar-tester.js
@@ -1,7 +1,16 @@
 var assert = require('assert'),
-    lib = require('../node_modules/lumbar/test/lib'),
-    lumbarTester = require('../lib/lumbar-tester')({includeTests: true}),
-    lumbarNoTester = require('../lib/lumbar-tester')({includeTests: false});
+    lib = require('../node_modules/lumbar/test/lib');
 
-exports['test-modules'] = lib.runTest('test/artifacts/test-modules.json', 'test/expected/test-modules', {plugins: [lumbarTester]});
-exports['no-test'] = lib.runTest('test/artifacts/test-modules.json', 'test/expected/no-tests', {plugins: [lumbarNoTester]});
+describe('lumbar-tester', function() {
+  describe('test mode', function() {
+    var lumbarTester = require('../lib/lumbar-tester')({includeTests: true});
+
+    it('should include tests', lib.runTest('test/artifacts/test-modules.json', 'test/expected/test-modules', {plugins: [lumbarTester]}));
+  });
+
+  describe('no-test mode', function() {
+    var lumbarNoTester = require('../lib/lumbar-tester')({includeTests: false});
+
+    it('should ignore test files', lib.runTest('test/artifacts/test-modules.json', 'test/expected/no-tests', {plugins: [lumbarNoTester]}));
+  });
+});
